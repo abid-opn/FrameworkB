ff0c5482b5133d32367b7195936c99d41f7d57ed
Abid Rahman <abid@opn.ooo>
2021-09-20 14:41:07 +0600

    first

---
+++ Sources/FrameworkB/FrameworkB.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Abid Rahman on 20/9/21.
+//
+
+import Foundation
+
+public class FrameworkB {
+    public static func use() {
+        print("Using Framework B version 0.1")
+    }
+}
